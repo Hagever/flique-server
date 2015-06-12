396e3930d594a96e2d5e03e4fa3c57e55b9cb28a
Gal Schlezinger <galstar@spitfire.co.il>
2015-06-12 23:29:31 +0300

    YES YESY YESYSSESE

--- test/call_babel.js
+++ test/call_babel.js
@@ -1,1 +1,3 @@
+var path = require('path');
 require('babel/register')({ stage: 0 });
+require('app-module-path').addPath(path.resolve(__dirname, '../lib'));
--- test/user-actions-spec.js
+++ test/user-actions-spec.js
@@ -1,23 +1,55 @@
-const UserActions = require('../lib/actions/user');
+const UserActions = require('actions/user');
 
 describe('UserActions', () => {
-  it('should work if the password and username are correct', (done) => {
-    UserActions.findByUsernameAndPassword({ username: 'test', password: 'test123' }).then(() => done()).catch(done);
-  });
+  describe('login', () => {
+    it('should work if the password and username are correct', (done) => {
+      UserActions.login({ username: 'test', password: 'test123' }).then(() => done()).catch(done);
+    });
 
-  it('should throw if the password is incorrect', (done) => {
-    UserActions.findByUsernameAndPassword({ username: 'test', password: 'something else' }).then(done).catch(() => done());
-  });
+    it('should throw if the password is incorrect', (done) => {
+      UserActions.login({ username: 'test', password: 'something else' }).then(done).catch(() => done());
+    });
 
-  it('should throw if there is no user', (done) => {
-    UserActions.findByUsernameAndPassword({ username: 'someoneee elseeee', password: 'whatever' }).then(done).catch(() => done());
-  });
+    it('should throw if there is no user', (done) => {
+      UserActions.login({ username: 'someoneee elseeee', password: 'whatever' }).then(done).catch(() => done());
+    });
 
-  it('user should not be case sensitive', (done) => {
-    UserActions.findByUsernameAndPassword({ username: 'TEST', password: 'test123' }).then(() => done()).catch(done);
-  });
+    it('user should not be case sensitive', (done) => {
+      UserActions.login({ username: 'TEST', password: 'test123' }).then(() => done()).catch(done);
+    });
 
-  it('password should be case sensitive', (done) => {
-    UserActions.findByUsernameAndPassword({ username: 'test', password: 'TEST123' }).then(done).catch(() => done());
+    it('password should be case sensitive', (done) => {
+      UserActions.login({ username: 'test', password: 'TEST123' }).then(done).catch(() => done());
+    });
+
+    describe('tokens', () => {
+      var token;
+      before((done) => {
+        UserActions.login({ username: 'test', password: 'test123' }).then(user => {
+          token = user.token;
+          done();
+        });
+      });
+
+      it('should find the user by the given token', (done) => {
+        UserActions.findByToken({ token }).then(() => {
+          done();
+        }).catch(done);
+      });
+
+      it('should not find the user by the wrong token', (done) => {
+        UserActions.findByToken({ token: 'abanibi' }).then(done).catch(() => done());
+      });
+    });
+
+    describe('find a user', () => {
+      it('should throw when user not found', (done) => {
+        UserActions.findByUsername('whattttt').then(done).catch(() => done());
+      });
+
+      it('should find the user when its okay', (done) => {
+        UserActions.findByUsername('test').then(() => done()).catch(done);
+      });
+    });
   });
 });
